3106f703abd738a671405cf4f7836130295e265d
Vis Vasanth <viscodesdpt@gmail.com>
2016-12-01 20:25:15 -0600

    detailed instructions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,47 +1,47 @@
 $('button').on('click', function() {
-    var car = $(this).data('car');
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=dc6zaTOxFJmzC&limit=10";
+var car = $(this).data('car');
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=dc6zaTOxFJmzC&limit=10"; 
 
 
-    $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
-        .done(function(response) {
-            console.log(response);
-            //response.data because image info is inside the data key
-            var results = response.data;
+$.ajax({
+	url: queryURL,
+	method: 'GET'
+})
+    .done(function(response){
+    	console.log (response);
+        //response.data because image info is inside the data key
+        var results = response.data;
 
-            //for loop to display only 10 results
-            for (var i = 0; i < results.length; i++) {
-                var carDiv = $('<div class = "item">');
-                var rating = results[i].rating;
-                var p = $('<p>').text("Rating: " + rating);
+        //for loop to display only 10 results
+        for(var i =0; i<results.length; i++){
+        	var carDiv = $('<div class = "item">');       	
+        	var rating = results[i].rating;
+        	var p = $('<p>').text("Rating: " + rating);
+        		
+        	var img = $('<img>');
+        	img.attr('src', results[i].images.fixed_height_still.url);
 
-                var img = $('<img>');
-                img.attr('src', results[i].images.fixed_height_still.url);
+            //DATA STATES added to img tag
+            img.attr('data-still', results[i].images.fixed_height_still.url);
+            img.attr('data-animate', results[i].images.fixed_height.url); 
+            img.attr('data-state', 'still');
+            img.attr('class', 'carImages');
+            console.log(img);
 
-                //DATA STATES added to img tag
-                img.attr('data-still', results[i].images.fixed_height_still.url);
-                img.attr('data-animate', results[i].images.fixed_height.url);
-                img.attr('data-state', 'still');
-                img.attr('class', 'carImages');
-                console.log(img);
-
-                carDiv.append(p);
-                carDiv.append(img);
-                $('#gifsAppearHere').prepend(carDiv);
-            }
-        });
+        	carDiv.append(p);
+        	carDiv.append(img);
+        	$('#gifsAppearHere').prepend(carDiv);
+        }
+    });
 });
 
 //=====================DATA STATE IMG CLICK======================
 $(document).on('click', '.carImages', dataState); //GOLDEN LINE!!!
 
-function dataState() {
+function dataState(){
     console.log('on click works');
     var state = $(this).attr('data-state');
-    console.log("The state is:" + state);
+        console.log("The state is:"+state);
 
     if (state == 'still') {
         $(this).attr('src', $(this).attr('data-animate'));
@@ -50,7 +50,7 @@ function dataState() {
     } else {
         $(this).attr('src', $(this).attr('data-still'));
         $(this).attr('data-state', 'still');
-    }
+    } 
 };
 
 //=====================SEARCH BUTTON ON CLICK FUNCTIONWITH NEW BUTTON=====================
@@ -63,37 +63,37 @@ $('#searchBtn').on('click', function() {
     console.log(newURL);
 
     //=============== ADD a button with search term ======================
-    var searchResultButton = ('<button>' + queryTerm + '</button>')
+    var searchResultButton = ('<button>'+ queryTerm+ '</button>')
     $('.searchedCarButtons').append(searchResultButton);
 
     //add function to the new button
-    $('.searchedCarButtons').on("click", function() {
+    $('.searchedCarButtons').on("click", function(){
         $.ajax({
-                url: newURL,
-                method: 'GET'
-            })
-            .done(function(response) {
-                console.log(response);
-                var sResults = response.data;
+            url:newURL,
+            method: 'GET'
+        })
+        .done(function(response){
+             console.log (response);
+             var sResults = response.data;
 
-                for (var i = 0; i < sResults.length; i++) {
-                    var sCarDiv = $('<div class = "item">');
-                    var sRating = sResults[i].rating;
-                    var sP = $('<p>').text("Rating: " + sRating);
+             for (var i=0; i<sResults.length; i++){
+                var sCarDiv = $('<div class = "item">');
+                var sRating = sResults[i].rating;
+                var sP = $('<p>').text("Rating: " + sRating);
+                
+                var sImg = $('<img>');
+                sImg.attr('src', sResults[i].images.fixed_height_still.url);
+                sImg.attr('data-still', sResults[i].images.fixed_height_still.url);
+                sImg.attr('data-animate', sResults[i].images.fixed_height.url); 
+                sImg.attr('data-state', 'still');
+                sImg.attr('class', 'carImages');
 
-                    var sImg = $('<img>');
-                    sImg.attr('src', sResults[i].images.fixed_height_still.url);
-                    sImg.attr('data-still', sResults[i].images.fixed_height_still.url);
-                    sImg.attr('data-animate', sResults[i].images.fixed_height.url);
-                    sImg.attr('data-state', 'still');
-                    sImg.attr('class', 'carImages');
+                
+                sCarDiv.append(sP);
+                sCarDiv.append(sImg);
+                $('#gifsAppearHere').prepend(sCarDiv);
 
-
-                    sCarDiv.append(sP);
-                    sCarDiv.append(sImg);
-                    $('#gifsAppearHere').prepend(sCarDiv);
-
-                }
-            });
+             }
+        });
     });
 });
